f19a383c242871bfdea7f59c5a8bdab4f7a688ef
justinian336 <jnelsonm64@gmail.com>
2016-03-13 08:03:53 +0000

    Fixed several bugs and reduced memory usage

--- mdnCrawl.js
+++ mdnCrawl.js
@@ -2,9 +2,10 @@ var jsonfile = require('jsonfile');
 var request = require('request');
 var cheerio = require('cheerio');
 
+//Insert here the name of the output file, the start-point urls and the topic routes to scrape:
 var fileName ='routes.json';
-var routes = [{enUrl:'https://developer.mozilla.org/en/docs/Web/JavaScript'}];
-var newLink = [];
+var routes = ['https://developer.mozilla.org/en/docs/Web/JavaScript'];
+var topicRoutes = ['Web/JavaScript'];
 
 var editLink = function(link){
     var newLink;
@@ -17,12 +18,16 @@ var editLink = function(link){
     return newLink;
 };
 
+var unleak = function(s){
+    return (' '+s).substr(1);
+};
+
+
 var j1 = 0;
 var j2 = 0;
 var j3 = 0;
 
 var stage3 = function(routesArr){
-    // Reset the languages array.
     request(routesArr[j3].esUrl, function(error, response, html){
     
         if(!error){
@@ -30,10 +35,10 @@ var stage3 = function(routesArr){
             // Get the html for the current route
             var $ = cheerio.load(html);
             
-            // Enumerate the languages available for the current route
+            // Obtain the languages available for the current route
             var message = $('.translationInProgress');
-            //console.log(message.children().first().text());
             routesArr[j3].esMessage = message.children().first().text();
+            console.log(routesArr[j3].esMessage);
             j3++;
                 
             if(j3==routes.length){
@@ -45,7 +50,21 @@ var stage3 = function(routesArr){
                 return;
             }
             else{
-                console.log(j3);
+                stage3(routesArr);
+            }
+        }
+        else{
+            console.log("No Spanish Route");
+            j3++;
+            if(j3==routes.length){
+                jsonfile.writeFile(fileName, routesArr, function(err){
+                    if(!err){
+                        console.log('Done! See '+fileName);
+                    }
+                });
+                return;
+            }
+            else{
                 stage3(routesArr);
             }
         }
@@ -68,6 +87,7 @@ var stage2 = function(routesArr){
                 //console.log($(lang).attr('href'));
                 if($(lang).text()=="Español"){
                     routesArr[j2].esUrl=editLink($(lang).attr('href'));
+                    console.log(routesArr[j2].esUrl);
                 }
             });
             //console.log(langRoutes);
@@ -78,7 +98,7 @@ var stage2 = function(routesArr){
                 stage3(routesArr);
             }
             else{
-                console.log(j2);
+                //console.log(j2);
                 stage2(routesArr);
             }
         }
@@ -86,44 +106,36 @@ var stage2 = function(routesArr){
 };
 
 var stage1 = function(routesArr){
-    // Reset the languages array.
-    request(routesArr[j1].enUrl, function(error, response, html){
     
+    request(routesArr[j1], function(error, response, html){
         if(!error){
-            
             // Get the html for the current route
             var $ = cheerio.load(html);
-            
-            // Enumerate the languages available for the current route
-            $('#translations').filter(function(){
-                
-                //Populate the routes array with the <a> tags found in the article.
-                $(this).filter(function(){
-                    var links = $('#wikiArticle a');
-                    links.each(function(i,link){
-                        if($(link).attr('href')!=undefined){
-                            if($(link).attr('href').indexOf("/en-US/docs")!=-1){
-                                newLink = editLink($(link).attr('href'));
-                                if(routesArr.indexOf({enUrl:newLink})==-1){
-                                    //console.log(newLink);
-                                    routesArr.push({enUrl:(" "+newLink).substr(1)});
-                                }
-                            }
+
+            var links = $('#wikiArticle a');
+            links.each(function(i,link){
+                if($(link).attr('href')!=undefined){
+                    if($(link).attr('href').indexOf("/en-US/docs/"+topicRoutes)!=-1){
+                        var newLink = editLink(unleak($(link).attr('href')));
+                        if(routesArr.indexOf(newLink)==-1){
+                            console.log(newLink);
+                            routesArr.push(newLink);
                         }
-                    });
-                });
-                //console.log(routes);
-                j1++;
-                
-                if(j1==routesArr.length-1){
-                    console.log('Moving to stage 2');
-                    stage2(routesArr);
-                }
-                else{
-                    console.log(j1);
-                    stage1(routesArr);
+                    }
                 }
             });
+            j1++;
+            
+            if(j1==routesArr.length-1){
+                for(var i=0;i<routesArr.length;i++){
+                    routes[i] = {enUrl:routes[i],esUrl:"",esMessage:""};
+                }
+                console.log('Moving to stage 2');
+                stage2(routesArr);
+            }
+            else{
+                stage1(routesArr);
+            }
         }
     });
 };
